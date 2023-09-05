f3a64323293ca781e02039b85e22dd4550463745
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-05 13:30:02 +0900

    🔧 chore: 초기 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles.style';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
